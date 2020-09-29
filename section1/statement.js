119fd42dabac2c8c8dc8998a88e56cc3ad178e7e
rotelstift <rotelstift@gmail.com>
2020-09-29 19:27:25 +0900

    rename result

--- section1/statement.js
+++ section1/statement.js
@@ -1,6 +1,28 @@
 const plays = require("./plays.json");
 const invoices = require("./invoices.json");
 
+function amountFor(perf, play) {
+    let result = 0;
+    switch (play.type) {
+        case "tragedy":
+            result = 40000;
+            if (perf.audience > 30) {
+                result += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            result = 30000;
+            if (perf.audience > 20) {
+                result += 10000 + 500 * (perf.audience - 20);
+            }
+            result += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return result;
+}
+
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -17,25 +39,7 @@ function statement (invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-        case "tragedy":
-            thisAmount = 40000;
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
-            }
-            break;
-        case "comedy":
-            thisAmount = 30000;
-            if (perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience - 20);
-            }
-            thisAmount += 300 * perf.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play);
 
         // ボリューム特典のポイントを加算
         volumeCredits += Math.max(perf.audience - 30, 0);
