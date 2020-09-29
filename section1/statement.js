9631507df3f2d01ce69052af13331f185cd74a0d
rotelstift <rotelstift@gmail.com>
2020-09-29 20:17:38 +0900

    move sub functions in main function

--- section1/statement.js
+++ section1/statement.js
@@ -1,32 +1,6 @@
 const plays = require("./plays.json");
 const invoices = require("./invoices.json");
 
-function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
-}
-
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -56,6 +30,32 @@ function statement (invoice, plays) {
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
 }
 
 console.log(statement(invoices[0], plays));
