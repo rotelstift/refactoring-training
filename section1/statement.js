faa0f964897e4c6d7247327adfd5f99e873ae426
rotelstift <rotelstift@gmail.com>
2020-09-30 08:49:00 +0900

    slide statement

--- section1/statement.js
+++ section1/statement.js
@@ -3,7 +3,6 @@ const invoices = require("./invoices.json");
 
 function statement (invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`
 
     for (let perf of invoice.performances) {
@@ -11,6 +10,7 @@ function statement (invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
+    let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
