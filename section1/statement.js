bdd43ab7fd66ae9888923605c84f7bd8f22702e9
rotelstift <rotelstift@gmail.com>
2020-09-29 20:07:10 +0900

    delete forgotten line

--- section1/statement.js
+++ section1/statement.js
@@ -42,7 +42,6 @@ function statement (invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf);
 
         // ボリューム特典のポイントを加算
         volumeCredits += Math.max(perf.audience - 30, 0);
