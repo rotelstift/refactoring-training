3398e8fa053ae6d05f893416ef2d8cb973381dd7
rotelstift <rotelstift@gmail.com>
2020-09-30 09:05:58 +0900

    make new function

--- section1/statement.js
+++ section1/statement.js
@@ -10,10 +10,7 @@ function statement (invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
+    let volumeCredits = totalVolumeCredits();
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
@@ -27,6 +24,14 @@ function statement (invoice, plays) {
                 minimumFractionDigits: 2
             }
         ).format(aNumber/100);
+    }
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+        return volumeCredits;
     }
 
     function volumeCreditsFor(aPerformance) {
